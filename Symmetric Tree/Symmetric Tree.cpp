b3ece5e16ac8f51257b36e891ed68f3a2ca1823d
octopopo <godpopo34@gmail.com>
2018-04-28 13:07:39 -0700

    Symmetric Tree
    Use recursion to go thorough the tree in a mirror way,
    The tricky part is to call the recursion.
    
    For this since the depth of the tree will be log n, the total running time will be O(log n)

---
+++ Symmetric Tree/Symmetric Tree.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        //careful for the null case
+        if(!root)
+            return true;
+        return checkSymmetric(root->left, root->right);
+    }
+    
+    bool checkSymmetric(TreeNode *node1, TreeNode *node2){
+        
+        if(!node1 && !node2){
+            return true;
+        }
+        else if((!node1 && node2) || (node1 && !node2))
+            return false;
+        else{
+            if(node1->val != node2->val)
+                return false;
+            else{
+                return checkSymmetric(node1->left, node2->right) && checkSymmetric(node1->right, node2->left);
+            }
+        }
+        return true;
+    }
+};
